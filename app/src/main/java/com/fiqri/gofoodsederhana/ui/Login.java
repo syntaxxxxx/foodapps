07bf93350109182d64114b8b3238a1436d9f1636
Fiqri Hafzain <fiqrihafzainislami@gmail.com>
2019-01-17 14:27:36 +0700

    update user login & session

--- app/src/main/java/com/fiqri/gofoodsederhana/ui/Login.java
+++ app/src/main/java/com/fiqri/gofoodsederhana/ui/Login.java
@@ -3,17 +3,25 @@ package com.fiqri.gofoodsederhana.ui;
 import android.os.Bundle;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
+import com.fiqri.gofoodsederhana.FoodUtama;
 import com.fiqri.gofoodsederhana.R;
 import com.fiqri.gofoodsederhana.helper.SessionManager;
+import com.fiqri.gofoodsederhana.model.ResponseRegister;
+import com.fiqri.gofoodsederhana.model.User;
+import com.fiqri.gofoodsederhana.network.ConfigRetrofit;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class Login extends SessionManager {
 
@@ -56,10 +64,62 @@ public class Login extends SessionManager {
                 levelUser = "User Biasa";
                 break;
             case R.id.sign_in:
+                validasiInputan();
                 break;
             case R.id.register:
                 intent(Register.class);
                 break;
         }
     }
+
+    private void validasiInputan() {
+
+        // tampung dalam variable dan ambil inputan di field nya
+        userName = loginUsername.getText().toString().trim();
+        password = loginPassword.getText().toString().trim();
+
+        if (TextUtils.isEmpty(userName)) {
+            loginUsername.requestFocus();
+            loginUsername.setError("nama tidak boleh kosong");
+
+        } else if (TextUtils.isEmpty(password)) {
+            loginPassword.requestFocus();
+            loginPassword.setError("password tidak boleh kosong");
+
+        } else {
+            sendRequestLogin();
+        }
+    }
+
+    private void sendRequestLogin() {
+        ConfigRetrofit.getInstance().login(
+                userName,
+                password,
+                levelUser
+        ).enqueue(new Callback<ResponseRegister>() {
+            @Override
+            public void onResponse(Call<ResponseRegister> call, Response<ResponseRegister> response) {
+
+                if (response.isSuccessful()) {
+                    String result = response.body().getResult();
+                    String msg = response.body().getMsg();
+                    String idUser = response.body().getUser().getIdUser();
+
+                    if (result.equals("1")){
+                        sessionManager.createSession(userName);
+                        sessionManager.setIdUser(idUser);
+                        intent(FoodUtama.class);
+                        shortToast(msg);
+                        finish();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseRegister> call, Throwable t) {
+                longToast(t.getMessage());
+            }
+        });
+    }
 }
+
